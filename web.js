46de9a053d30013b8da7f65bb03e0b9f15f90f6f
Stefano Costa <ridethepenguin@gmail.com>
2013-07-16 21:50:43 +0000

    web.js now reads content from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var indexContent = fs.readFileSync('index.html', { encoding : 'utf8' });
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(indexContent);    
 });
 
 var port = process.env.PORT || 5000;
